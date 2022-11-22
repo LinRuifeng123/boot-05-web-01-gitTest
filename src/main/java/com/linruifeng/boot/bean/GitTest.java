8be2fe166789d3b8c2b516cb0a38452bc863b2a1
LinRuifeng <1346548401@qq.com>
2022-11-22 21:31:41 +0800

    second commit

---
+++ src/main/java/com/linruifeng/boot/bean/GitTest.java
@@ -0,0 +1,13 @@
+package com.linruifeng.boot.bean;
+
+/**
+ * @author linruifeng
+ * @create 2022-11-22 21:28
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
