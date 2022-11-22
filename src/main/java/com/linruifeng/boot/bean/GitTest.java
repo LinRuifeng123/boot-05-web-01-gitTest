9ef36565f554cb09315c67196802f18caa33321f
LinRuifeng <1346548401@qq.com>
2022-11-22 21:49:52 +0800

    hot-fix commit2

--- src/main/java/com/linruifeng/boot/bean/GitTest.java
+++ src/main/java/com/linruifeng/boot/bean/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("hot-fix test!");
     }
 }
